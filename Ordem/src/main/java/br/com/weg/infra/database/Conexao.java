4bd45d76cd52c341a4147377aa5758ba75a51396
Kaua Felix da Silva Costa <kfelixcosta11@gmail.com>
2026-02-13 16:44:18 -0300

    feat: adiciona metodo conectar para ter acesso ao banco de dados

--- Ordem/src/main/java/br/com/weg/infra/database/Conexao.java
+++ Ordem/src/main/java/br/com/weg/infra/database/Conexao.java
@@ -1,4 +1,18 @@
 package br.com.weg.infra.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
+
+    public static final String url = "jdbc:postgresql://dpg-d67mu4t6ubrc73967sq0-a.oregon-postgres.render.com/gestaotime";
+    public static final String user = "time";
+    public static final String pass = "G7wyPuKVzDb7eDUYOdoW2sXbfQNUq6jM";
+
+
+    public static Connection conectar () throws SQLException{
+
+        return DriverManager.getConnection(url, user, pass);
+    }
 }
