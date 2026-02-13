8d1a3c9620eee2adefc90ca71e61d53d1828e9d7
jonathan_uber <jonathanuber121207@gmail.com>
2026-02-13 16:15:30 -0300

    feat: cria DTOs

--- Ordem/src/main/java/br/com/weg/application/dto/Partida/PartidaResponseDTO.java
+++ Ordem/src/main/java/br/com/weg/application/dto/Partida/PartidaResponseDTO.java
@@ -1,4 +1,10 @@
 package br.com.weg.application.dto.Partida;
 
-public class PartidaResponseDTO {
+import java.time.LocalDateTime;
+
+public record PartidaResponseDTO (int id,
+                                  int idClubeA,
+                                  int idClubeB,
+                                  LocalDateTime dataHoraPartida,
+                                  String localizacao) {
 }
