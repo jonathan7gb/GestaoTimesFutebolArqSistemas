e4ad714491287aeae8a200079e452648f891811b
jonathan_uber <jonathanuber121207@gmail.com>
2026-02-13 16:21:12 -0300

    feat: implementa os mappers dos DTO

--- Ordem/src/main/java/br/com/weg/application/mapper/ClubeMapper.java
+++ Ordem/src/main/java/br/com/weg/application/mapper/ClubeMapper.java
@@ -1,4 +1,16 @@
 package br.com.weg.application.mapper;
 
+import br.com.weg.application.dto.Clube.ClubeRequestDTO;
+import br.com.weg.application.dto.Clube.ClubeResponseDTO;
+import br.com.weg.domain.entity.Clube;
+
 public class ClubeMapper {
+
+    public ClubeResponseDTO toDto(Clube clube){
+        return new ClubeResponseDTO(clube.getId(), clube.getNome(), clube.getAnoFundacao(), clube.getPais());
+    }
+
+    public Clube toEntity(ClubeRequestDTO clubeRequestDTO){
+        return new Clube(clubeRequestDTO.nome(), clubeRequestDTO.anoFundacao(), clubeRequestDTO.pais());
+    }
 }
