f0aed6b19ae21a555443e1dfb704ef96de3e842f
Kaua Felix <kfelixcosta11@gmail.com>
2026-02-13 19:22:07 -0300

    feat: adiciona metodos para serem implementados

--- Ordem/src/main/java/br/com/weg/domain/repository/ClubeRepository.java
+++ Ordem/src/main/java/br/com/weg/domain/repository/ClubeRepository.java
@@ -1,4 +1,12 @@
 package br.com.weg.domain.repository;
 
+import br.com.weg.domain.entity.Clube;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface ClubeRepository {
+    Clube salvarClube(Clube clube) throws SQLException;
+    List<Clube> listarClubes() throws SQLException;
+    Clube existeId(int id) throws SQLException;
 }
