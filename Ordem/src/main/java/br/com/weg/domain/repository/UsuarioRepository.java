e1ca4702fd1f976f4fbbb36cca6fcb1d6f275b39
KAUÃ FELIX DA SILVA COSTA <kaua_fs_costa@estudante.sesisenai.org.br>
2026-02-16 20:42:46 -0300

    feat: adiciona metodos no repository e faz algumas implementacoes

--- Ordem/src/main/java/br/com/weg/domain/repository/UsuarioRepository.java
+++ Ordem/src/main/java/br/com/weg/domain/repository/UsuarioRepository.java
@@ -1,4 +1,16 @@
 package br.com.weg.domain.repository;
 
+import br.com.weg.domain.entity.Usuario;
+import br.com.weg.domain.enums.TipoUsuario;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface UsuarioRepository {
+
+    Usuario criarUsuario (Usuario usuario) throws SQLException;
+    List<Usuario> listarUsuarios() throws SQLException;
+    List<Usuario> listarJogador(TipoUsuario tipoUsuario) throws SQLException;
+    List<Usuario> listarComissaoTecnica() throws SQLException;
+    List<Usuario> listarPresidente() throws SQLException;
 }
--- Ordem/src/main/java/br/com/weg/infra/persistence/UsuarioRepositoryImpl.java
+++ Ordem/src/main/java/br/com/weg/infra/persistence/UsuarioRepositoryImpl.java
@@ -1,6 +1,136 @@
 package br.com.weg.infra.persistence;
 
+import br.com.weg.domain.entity.Usuario;
+import br.com.weg.domain.enums.TipoUsuario;
 import br.com.weg.domain.repository.UsuarioRepository;
+import br.com.weg.infra.database.Conexao;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsuarioRepositoryImpl implements UsuarioRepository {
+
+
+    @Override
+    public Usuario criarUsuario(Usuario usuario) throws SQLException {
+
+        String sql = """
+                INSERT INTO usuario (
+                nome, 
+                peso,
+                altura,
+                tipo,
+                id_clube )
+                VALUES (?,?,?,?,?)
+                """;
+
+        try(Connection conn = Conexao.conectar();
+            PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
+
+            ps.setString(1, usuario.getNome());
+            ps.setDouble(2, usuario.getPeso());
+            ps.setInt(3, usuario.getAltura());
+            ps.setString(4, usuario.getTipo().name());
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+
+            if (rs.next()){
+                int idGerado = rs.getInt(1);
+                usuario.setId(idGerado);
+            }
+        }
+        return usuario;
+    }
+
+    @Override
+    public List<Usuario> listarUsuarios() throws SQLException {
+
+        List<Usuario> usuarios = new ArrayList<>();
+
+        String sql = """
+                
+                SELECT 
+                id,
+                nome,
+                peso,
+                altura,
+                tipo,
+                id_clube 
+                FROM
+                usuario
+                """;
+
+        try(Connection conn = Conexao.conectar();
+        PreparedStatement ps = conn.prepareStatement(sql)){
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()){
+                Usuario usuario = new Usuario(
+                        rs.getInt("id"),
+                        rs.getString("nome"),
+                        rs.getDouble("peso"),
+                        rs.getInt("altura"),
+                        (TipoUsuario) rs.getObject("tipo"),
+                        rs.getInt("id_clube")
+                );
+                usuarios.add(usuario);
+                return usuarios;
+            }
+        }
+
+        return List.of();
+    }
+
+    @Override
+    public List<Usuario> listarJogador(TipoUsuario tipoUsuario) throws SQLException {
+
+        List<Usuario> usuarios = new ArrayList<>();
+
+        String sql = """
+                SELECT 
+                id,
+                nome,
+                peso, 
+                altura,
+                tipo,
+                id_clube
+                FROM 
+                usuario
+                WHERE tipo = ?::user_type
+                """;
+        try (Connection conn = Conexao.conectar();
+        PreparedStatement ps = conn.prepareStatement(sql)){
+            ps.setString(1, tipoUsuario.name());
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()){
+                Usuario usuario = new Usuario(
+                        rs.getInt("id"),
+                        rs.getString("nome"),
+                        rs.getDouble("peso"),
+                        rs.getInt("altura"),
+                        (TipoUsuario) rs.getObject("tipo"),
+                        rs.getInt("id_clube")
+                );
+                usuarios.add(usuario);
+                return usuarios;
+            }
+        }
+
+        return List.of();
+    }
+
+    @Override
+    public List<Usuario> listarComissaoTecnica() throws SQLException {
+        return List.of();
+    }
+
+    @Override
+    public List<Usuario> listarPresidente() throws SQLException {
+        return List.of();
+    }
 }
