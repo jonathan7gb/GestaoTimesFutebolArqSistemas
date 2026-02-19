73e0f8a30b09b7d384e9817b648e781fc89f0254
Kaua Felix <kfelixcosta11@gmail.com>
2026-02-19 19:50:44 -0300

    feat: adiciona opção para listar partidas por clube no menu de usuários e comissões

--- Ordem/src/main/java/br/com/weg/presentation/controller/MainController.java
+++ Ordem/src/main/java/br/com/weg/presentation/controller/MainController.java
@@ -3,6 +3,7 @@ package br.com.weg.presentation.controller;
 
 import br.com.weg.application.dto.Clube.ClubeRequestDTO;
 import br.com.weg.application.dto.Partida.PartidaRequestDTO;
+import br.com.weg.application.dto.Partida.PartidaResponseDTO;
 import br.com.weg.application.dto.Usuario.UsuarioRequestDTO;
 import br.com.weg.application.dto.Usuario.UsuarioResponseDTO;
 import br.com.weg.application.service.ClubeService;
@@ -38,13 +39,48 @@ public class MainController {
 
         switch (escolhaPrincipal){
             case 1 -> {
-                int escolhaJogador = usuarioView.jogadorMenu();
-                System.out.println();
+                do {
+                    int escolhaJogador = usuarioView.jogadorMenu();
+                    System.out.println();
 
+                    switch (escolhaJogador) {
+                        case 1 -> {
+                            int idClube = clubeView.pedirIdClube();
+                            List<UsuarioResponseDTO> listarJogadoresPorClube = usuarioService.listarUsuariosDeUmClube(idClube);
+                            usuarioView.listarUsuarios(listarJogadoresPorClube, usuarioService.idsNomeClubeUsuario());
+                        }
+                        case 2 -> {
+                            int idClube = clubeView.pedirIdClube();
+                            List<PartidaResponseDTO> listarPartidasPorClube = partidaService.listarPartidas();
+                            partidaView.listarPartidas(listarPartidasPorClube, partidaService.retornarNomesClubesPartidas());
+                        }
+                        case 0 -> {
+                            return;
+                        }
+                    }
+                }while (true);
             }
             case 2 -> {
-                int escolhaComissao = usuarioView.comissaoMenu();
-                System.out.println();
+                do {
+                    int escolhaComissao = usuarioView.comissaoMenu();
+                    System.out.println();
+
+                    switch (escolhaComissao){
+                        case 1 -> {
+                            int idClube = clubeView.pedirIdClube();
+                            List<UsuarioResponseDTO> listarJogadoresPorClube = usuarioService.listarUsuariosDeUmClube(idClube);
+                            usuarioView.listarUsuarios(listarJogadoresPorClube,  usuarioService.idsNomeClubeUsuario());
+                        }
+                        case 2 -> {
+                            int idClube = clubeView.pedirIdClube();
+                            List<PartidaResponseDTO> listarPartidasPorClube = partidaService.listarPartidas();
+                            partidaView.listarPartidas(listarPartidasPorClube, partidaService.retornarNomesClubesPartidas());
+                        }
+                        case 0 -> {
+                            return;
+                        }
+                    }
+                }while (true);
             }
             case 3 -> {
                 do{
@@ -63,6 +99,11 @@ public class MainController {
                             int idClube = clubeView.pedirIdClube();
                             List<UsuarioResponseDTO>listarUsuariosPorClube = usuarioService.listarUsuariosDeUmClube(idClube);
                             usuarioView.listarUsuarios(listarUsuariosPorClube,  usuarioService.idsNomeClubeUsuario());
+                        }
+                        case 4 -> {
+                            int idClube = clubeView.pedirIdClube();
+                            List<PartidaResponseDTO> listarPartidasPorClube = partidaService.listarPartidas();
+                            partidaView.listarPartidas(listarPartidasPorClube, partidaService.retornarNomesClubesPartidas());
                         }
                         case 0 -> {
                             return;
