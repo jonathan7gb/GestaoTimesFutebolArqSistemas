427268eb38b4d982bf61cc7fd493974be57a113c
jonathan_uber <jonathanuber121207@gmail.com>
2026-02-27 15:10:34 -0300

    fix: remove unnecessary comments

--- Ordem/src/main/java/br/com/weg/infra/notification/SilentNotificacao.java
+++ Ordem/src/main/java/br/com/weg/infra/notification/SilentNotificacao.java
@@ -2,24 +2,17 @@ package br.com.weg.infra.notification;
 
 import br.com.weg.domain.notification.INotificacao;
 
-/**
- * Estratégia concreta: suprime todas as notificações.
- * Útil para execução silenciosa ou cenários de teste automatizado.
- */
 public class SilentNotificacao implements INotificacao {
 
     @Override
     public void sucesso(String mensagem) {
-        // silencioso – sem saída
     }
 
     @Override
     public void erro(String mensagem) {
-        // silencioso – sem saída
     }
 
     @Override
     public void info(String mensagem) {
-        // silencioso – sem saída
     }
 }
