bbd2b1f67c47ae3ef833fb27da45285cc4b914bd
Kauã Felix <kaua_fs_costa@estudante.sesisenai.org.br>
2026-02-27 16:21:37 -0300

    refactor: reorganiza a classe Clube e a classe Main para piorar a separação de responsabilidades e a legibilidade do código

--- Caos/src/main/java/br/com/weg/MembroFutebol.java
+++ Caos/src/main/java/br/com/weg/MembroFutebol.java
@@ -1,10 +1,36 @@
 package br.com.weg;
 
-//Violação do ISP: Nenhuma classe deve ser forçada a depender de métodos que não utiliza.
+/**
+ * VIOLAÇÃO MONOLÍTICA DA INTERFACE SEGREGATION PRINCIPLE (ISP)
+ *
+ * Esta interface força TODAS as implementações a ter TODOS esses métodos,
+ * mesmo que um Presidente nunca jogue futebol ou um Jogador nunca contrate ninguém.
+ *
+ * ISP viola = Forçar clientes a depender de métodos que não usam
+ */
 public interface MembroFutebol {
+
+    // Métodos que só Jogadores usam
     void jogar();
     void treinar();
+    void cobrarEscanteio();
+    void marcarGol();
+    void sofrerCartao();
+
+    // Métodos que só Administradores/Presidentes usam
     void contratar();
     void demitir();
-    void cobrarEscanteio();
+    void negocias();
+
+    // Métodos gerais
+    void ganharSalario();
+    void receberBonus();
+    void serfericado();
+
+    // Métodos estranhos misturados
+    void apareceNaImprensa();
+    void assinarAutografo();
+    void brigarNoTreino();
+    void chorarNaDerrota();
+    void dormir();
 }
